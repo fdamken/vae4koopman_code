7c679d3faa00446daf72fec34f4f0aabb0eec224
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2020-05-01 23:12:16 +0200

    Renamed variables of the forward pass.

--- src/orig/kalmansmooth.py
+++ src/orig/kalmansmooth.py
@@ -3,109 +3,92 @@ import numpy as np
 
 
 def kalmansmooth(A, C, Q, R, x0, P0, Y):
-    (N, p, T) = Y.shape
-    K = len(x0)
-    tiny = np.exp(-700)
-    I = np.eye(K)
-    const = (2 * np.pi) ** (-p / 2)
-    problem = 0
-    lik = 0
+    (no_observation_sequences, observation_dim, T) = Y.shape
+    state_dim = len(x0)
+    I = np.eye(state_dim)
+    likelihood = 0
 
-    Xpre = np.zeros((N, K))
-    Xcur = np.zeros((N, K, T))
-    Xfin = np.zeros((N, K, T))
-
-    Ppre = np.zeros((K, K, T))
-    Pcur = np.zeros((K, K, T))
-    Pfin = np.zeros((K, K, T))
-
-    Pt = np.zeros((K, K))
-    Pcov = np.zeros((K, K))
-    Kcur = np.zeros((K, p))
-    invP = np.zeros((p, p))
-    J = np.zeros((K, K, T))
+    m = np.zeros((no_observation_sequences, state_dim, T))
+    P = np.zeros((state_dim, state_dim, T))
+    V = np.zeros((state_dim, state_dim, T))
+    J = np.zeros((state_dim, state_dim, T))
 
     #
     # Forward pass.
 
-    R = R + (R == 0) * tiny
+    R = R + (R == 0) * np.exp(-700)
 
-    Xpre = np.ones((N, 1)) @ x0.T
-    Ppre[:, :, 0] = P0
-    invR = np.diag(1 / R)
-
+    # Initialize the forward pass.
+    m_pre = np.ones((no_observation_sequences, 1)) @ x0.T
+    P[:, :, 0] = P0
     for t in range(0, T):
-        if K < p:
-            # TODO
-            # temp1=rdiv(C,R);
-            # temp2=temp1*Ppre(:,:,t); % inv(R)*C*Ppre
-            # temp3=C'*temp2;
-            # temp4=inv(I+temp3)*temp1';
-            # invP=invR-temp2*temp4;
-            # CP= temp1' - temp3*temp4;    % C'*invP
-            raise Exception('Not yet implemented!')
-        else:
-            temp1 = np.diag(R) + C @ Ppre[:, :, t] @ C.T
-            invP = np.linalg.inv(temp1)
-            CP = C.T @ invP
+        # TODO: state_dim < observation_dim!
+        # invR = np.diag(1 / R)
+        # temp1 = C / R[:, np.newaxis]  # temp1=rdiv(C,R);
+        # temp2 = temp1 @ P[:, :, t]  # temp2=temp1*Ppre(:,:,t);
+        # temp3 = C.T @ temp2  # temp3=C'*temp2;
+        # temp4 = np.linalg.inv(I + temp3) @ temp1.T  # temp4=inv(I+temp3)*temp1';
+        # invP = invR - temp2 @ temp4  # invP=invR-temp2*temp4;
+        # CP = temp1.T - temp3 @ temp4  # CP= temp1' - temp3*temp4;
 
-        Kcur = Ppre[:, :, t] @ CP
-        KC = Kcur @ C
-        Ydiff = Y[:, :, t] - Xpre @ C.T
-        Xcur[:, :, t] = Xpre + Ydiff @ Kcur.T
-        Pcur[:, :, t] = Ppre[:, :, t] - KC @ Ppre[:, :, t]
+        if state_dim < observation_dim:
+            raise Exception('state_dim < observation_dim is not (yet) supported!')
+
+        invP = np.linalg.inv(C @ P[:, :, t] @ C.T + np.diag(R))
+        K = P[:, :, t] @ C.T @ invP
+        Ydiff = Y[:, :, t] - m_pre @ C.T
+        m[:, :, t] = m_pre + Ydiff @ K.T
+        V[:, :, t] = P[:, :, t] - K @ C @ P[:, :, t]
+
+        # Initialize the next pass (iff there is a next pass).
         if t < T - 1:
-            Xpre = Xcur[:, :, t] @ A.T
-            Ppre[:, :, t + 1] = A @ Pcur[:, :, t] @ A.T + Q
+            m_pre = m[:, :, t - 1] @ A.T
+            P[:, :, t + 1] = A @ V[:, :, t] @ A.T + Q
 
+        # Likelihood computation.
         detiP = np.sqrt(np.linalg.det(invP))
         if np.isreal(detiP) and detiP > 0:
-            lik = lik + N * np.log(detiP) - 0.5 * np.sum(np.sum(np.multiply(Ydiff, Ydiff @ invP), axis = 0), axis = 0)
+            likelihood = likelihood + no_observation_sequences * np.log(detiP) - 0.5 * np.sum(np.sum(np.multiply(Ydiff, Ydiff @ invP), axis = 0), axis = 0)
         else:
-            problem = 1
+            raise Exception('problem')
 
-    lik = lik + N * T * np.log(const)
+    likelihood = likelihood + no_observation_sequences * T * np.log((2 * np.pi) ** (-observation_dim / 2))
 
     #
     # Backward pass.
 
-    A1 = np.zeros((K, K))
-    A2 = np.zeros((K, K))
-    A3 = np.zeros((K, K))
-    Ptsum = np.zeros((K, K))
-    YX = np.zeros((p, K))
+    x_hat = np.zeros((no_observation_sequences, state_dim, T))
+    V_backward = np.zeros((state_dim, state_dim, T))
+
+    A1 = np.zeros((state_dim, state_dim))
 
     t = T - 1
-    Xfin[:, :, t] = Xcur[:, :, t]
-    Pfin[:, :, t] = Pcur[:, :, t]
-    Pt = Pfin[:, :, t] + Xfin[:, :, t].T @ Xfin[:, :, t] / N
+    x_hat[:, :, t] = m[:, :, t]
+    V_backward[:, :, t] = V[:, :, t]
+    Pt = V_backward[:, :, t] + x_hat[:, :, t].T @ x_hat[:, :, t] / no_observation_sequences
     A2 = -Pt
     Ptsum = Pt
 
-    YX = Y[:, :, t].T @ Xfin[:, :, t]
+    YX = Y[:, :, t].T @ x_hat[:, :, t]
 
     for t in reversed(range(0, T - 1)):
-        J[:, :, t] = Pcur[:, :, t] @ A.T @ np.linalg.inv(Ppre[:, :, t + 1])
-        Xfin[:, :, t] = Xcur[:, :, t] + (Xfin[:, :, t + 1] - Xcur[:, :, t] @ A.T) @ J[:, :, t].T
+        J[:, :, t] = V[:, :, t] @ A.T @ np.linalg.inv(P[:, :, t + 1])
+        x_hat[:, :, t] = m[:, :, t] + (x_hat[:, :, t + 1] - m[:, :, t] @ A.T) @ J[:, :, t].T
 
-        Pfin[:, :, t] = Pcur[:, :, t] + J[:, :, t] @ (Pfin[:, :, t + 1] - Ppre[:, :, t + 1]) @ J[:, :, t].T
-        Pt = Pfin[:, :, t] + Xfin[:, :, t].T @ Xfin[:, :, t] / N
+        V_backward[:, :, t] = V[:, :, t] + J[:, :, t] @ (V_backward[:, :, t + 1] - P[:, :, t + 1]) @ J[:, :, t].T
+        Pt = V_backward[:, :, t] + x_hat[:, :, t].T @ x_hat[:, :, t] / no_observation_sequences
         Ptsum = Ptsum + Pt
-        YX = YX + Y[:, :, t].T @ Xfin[:, :, t]
+        YX = YX + Y[:, :, t].T @ x_hat[:, :, t]
 
     A3 = Ptsum - Pt
     A2 = Ptsum + A2
 
     t = T - 1
-    Pcov = (I - KC) @ A @ Pcur[:, :, t - 1]
-    A1 = A1 + Pcov + Xfin[:, :, t].T @ Xfin[:, :, t - 1] / N
+    Pcov = (I - K @ C) @ A @ V[:, :, t - 1]
+    A1 = A1 + Pcov + x_hat[:, :, t].T @ x_hat[:, :, t - 1] / no_observation_sequences
 
     for t in reversed(range(1, T - 1)):
-        Pcov = (Pcur[:, :, t] + J[:, :, t] @ (Pcov - A @ Pcur[:, :, t])) @ J[:, :, t - 1].T
-        A1 = A1 + Pcov + Xfin[:, :, t].T @ Xfin[:, :, t - 1] / N
+        Pcov = (V[:, :, t] + J[:, :, t] @ (Pcov - A @ V[:, :, t])) @ J[:, :, t - 1].T
+        A1 = A1 + Pcov + x_hat[:, :, t].T @ x_hat[:, :, t - 1] / no_observation_sequences
 
-    if problem:
-        print('problem')
-        problem = 0
-
-    return lik, Xfin, Pfin, Ptsum, YX, A1, A2, A3
+    return likelihood, x_hat, V_backward, Ptsum, YX, A1, A2, A3
