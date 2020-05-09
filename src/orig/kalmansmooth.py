d03e1daa769fee30fa4cde30302b216d5351596a
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2020-05-09 13:42:32 +0200

    Removed Matlab code.

--- src/orig/kalmansmooth.py
+++ src/orig/kalmansmooth.py
@@ -2,109 +2,100 @@ import numpy as np
 
 
 
-# Returns: lik, Xfin, Pfin, Ptsum, YX, A1, A2, A3
-def kalmansmooth(A, C, Q, R, x0, P0, Y):  # function  [lik,Xfin,Pfin,Ptsum,YX,A1,A2,A3]=kalmansmooth(A,C,Q,R,x0,P0,Y);
-    (N, p, T) = Y.shape  # [N p T]=size(Y);
-    K = len(x0)  # K=length(x0);
-    tiny = np.exp(-700)  # tiny=exp(-700);
-    I = np.eye(K)  # I=eye(K);
-    const = (2 * np.pi) ** (-p / 2)  # const=(2*pi)^(-p/2);
-    problem = 0  # problem=0;
-    lik = 0  # lik=0;
+def kalmansmooth(A, C, Q, R, x0, P0, Y):
+    (N, p, T) = Y.shape
+    K = len(x0)
+    tiny = np.exp(-700)
+    I = np.eye(K)
+    const = (2 * np.pi) ** (-p / 2)
+    problem = 0
+    lik = 0
 
-    Xcur = np.zeros((N, K, T))  # Xcur=zeros(N,K,T);
-    Xfin = np.zeros((N, K, T))  # Xfin=zeros(N,K,T);
+    Xcur = np.zeros((N, K, T))
+    Xfin = np.zeros((N, K, T))
 
-    Ppre = np.zeros((K, K, T))  # Ppre=zeros(K,K,T);
-    Pcur = np.zeros((K, K, T))  # Pcur=zeros(K,K,T);
-    Pfin = np.zeros((K, K, T))  # Pfin=zeros(K,K,T);
+    Ppre = np.zeros((K, K, T))
+    Pcur = np.zeros((K, K, T))
+    Pfin = np.zeros((K, K, T))
 
-    J = np.zeros((K, K, T))  # J=zeros(K,K,T);
+    J = np.zeros((K, K, T))
 
     #
     # FORWARD PASS
 
-    R = R + (R == 0) * tiny  # R=R+(R==0)*tiny;
+    R = R + (R == 0) * tiny
 
-    Xpre = np.ones((N, 1)) @ x0.T  # Xpre=ones(N,1)*x0';
-    Ppre[:, :, 0] = P0  # Ppre(:,:,1)=P0;
-    invR = np.diag(1 / R)  # invR=diag(1./R);
+    Xpre = np.ones((N, 1)) @ x0.T
+    Ppre[:, :, 0] = P0
+    invR = np.diag(1 / R)
 
-    for t in range(0, T):  # for t=1:T
-        if K < p:  # if (K<p)
-            temp1 = C / R.reshape(-1, 1)  # temp1=rdiv(C,R);
-            temp2 = temp1 @ Ppre[:, :, t]  # temp2=temp1*Ppre(:,:,t); % inv(R)*C*Ppre
-            temp3 = C.T @ temp2  # temp3=C'*temp2;
-            temp4 = np.linalg.solve(I + temp3, temp1.T)  # temp4=inv(I+temp3)*temp1';
-            invP = invR - temp2 @ temp4  # invP=invR-temp2*temp4;
-            CP = temp1.T - temp3 @ temp4  # CP= temp1' - temp3*temp4;  % C'*invP
-        else:  # else
-            temp1 = np.diag(R) + C @ Ppre[:, :, t] @ C.T  # temp1=diag(R)+C*Ppre(:,:,t)*C';
-            invP = np.linalg.inv(temp1)  # invP=inv(temp1);
-            CP = C.T @ invP  # CP=C'*invP;
-        # end;
+    for t in range(0, T):
+        if K < p:
+            temp1 = C / R.reshape(-1, 1)
+            temp2 = temp1 @ Ppre[:, :, t]
+            temp3 = C.T @ temp2
+            temp4 = np.linalg.solve(I + temp3, temp1.T)
+            invP = invR - temp2 @ temp4
+            CP = temp1.T - temp3 @ temp4
+        else:
+            temp1 = np.diag(R) + C @ Ppre[:, :, t] @ C.T
+            invP = np.linalg.inv(temp1)
+            CP = C.T @ invP
 
-        Kcur = Ppre[:, :, t] @ CP  # Kcur=Ppre(:,:,t)*CP;
-        KC = Kcur @ C  # KC=Kcur*C;
-        Ydiff = Y[:, :, t] - Xpre @ C.T  # Ydiff=Y(:,:,t)-Xpre*C';
-        Xcur[:, :, t] = Xpre + Ydiff @ Kcur.T  # Xcur(:,:,t)=Xpre+Ydiff*Kcur';
-        Pcur[:, :, t] = Ppre[:, :, t] - KC @ Ppre[:, :, t]  # Pcur(:,:,t)=Ppre(:,:,t)-KC*Ppre(:,:,t);
-        if t < T - 1:  # if (t<T)
-            Xpre = Xcur[:, :, t] @ A.T  # Xpre=Xcur(:,:,t)*A';
-            Ppre[:, :, t + 1] = A @ Pcur[:, :, t] @ A.T + Q  # Ppre(:,:,t+1)=A*Pcur(:,:,t)*A'+Q;
-        # end;
+        Kcur = Ppre[:, :, t] @ CP
+        KC = Kcur @ C
+        Ydiff = Y[:, :, t] - Xpre @ C.T
+        Xcur[:, :, t] = Xpre + Ydiff @ Kcur.T
+        Pcur[:, :, t] = Ppre[:, :, t] - KC @ Ppre[:, :, t]
+        if t < T - 1:
+            Xpre = Xcur[:, :, t] @ A.T
+            Ppre[:, :, t + 1] = A @ Pcur[:, :, t] @ A.T + Q
 
-        # calculate likelihood
-        detP = np.linalg.det(invP)  # detiP=sqrt(det(invP));
-        if detP > 0:  # if (isreal(detiP) & detiP>0)
-            detiP = np.sqrt(detP)  # detiP=sqrt(det(invP));
-            lik = lik + N * np.log(detiP) - 0.5 * np.sum(np.sum(np.multiply(Ydiff, Ydiff @ invP), axis = 0), axis = 0)  # lik=lik+N*log(detiP)-0.5*sum(sum(Ydiff.*(Ydiff*invP)));
-        else:  # else
-            problem = 1  # problem=1;
-        # end;
-    # end;
+        detP = np.linalg.det(invP)
+        if detP > 0:
+            detiP = np.sqrt(detP)
+            lik = lik + N * np.log(detiP) - 0.5 * np.sum(np.sum(np.multiply(Ydiff, Ydiff @ invP), axis = 0), axis = 0)
+        else:
+            problem = 1
 
-    lik = lik + N * T * np.log(const)  # lik=lik+N*T*log(const);
+    lik = lik + N * T * np.log(const)
 
     #
     # BACKWARD PASS
 
-    A1 = np.zeros((K, K))  # A1=zeros(K);
+    A1 = np.zeros((K, K))
 
-    t = T - 1  # t=T;
-    Xfin[:, :, t] = Xcur[:, :, t]  # Xfin(:,:,t)=Xcur(:,:,t);
-    Pfin[:, :, t] = Pcur[:, :, t]  # Pfin(:,:,t)=Pcur(:,:,t);
-    Pt = Pfin[:, :, t] + Xfin[:, :, t].T @ Xfin[:, :, t] / N  # Pt=Pfin(:,:,t) + Xfin(:,:,t)'*Xfin(:,:,t)/N;
-    A2 = -Pt  # A2= -Pt;
-    Ptsum = Pt  # Ptsum=Pt;
+    t = T - 1
+    Xfin[:, :, t] = Xcur[:, :, t]
+    Pfin[:, :, t] = Pcur[:, :, t]
+    Pt = Pfin[:, :, t] + Xfin[:, :, t].T @ Xfin[:, :, t] / N
+    A2 = -Pt
+    Ptsum = Pt
 
-    YX = Y[:, :, t].T @ Xfin[:, :, t]  # YX=Y(:,:,t)'*Xfin(:,:,t);
+    YX = Y[:, :, t].T @ Xfin[:, :, t]
 
-    for t in reversed(range(0, T - 1)):  # for t=(T-1):-1:1
-        J[:, :, t] = np.linalg.solve(Ppre[:, :, t + 1], A @ Pcur[:, :, t]).T  # J(:,:,t)=Pcur(:,:,t)*A'*inv(Ppre(:,:,t+1));
-        Xfin[:, :, t] = Xcur[:, :, t] + (Xfin[:, :, t + 1] - Xcur[:, :, t] @ A.T) @ J[:, :, t].T  # Xfin(:,:,t)=Xcur(:,:,t)+(Xfin(:,:,t+1)-Xcur(:,:,t)*A')*J(:,:,t)';
-        # Pfin(:,:,t)=Pcur(:,:,t)+J(:,:,t)*(Pfin(:,:,t+1)-Ppre(:,:,t+1))*J(:,:,t)';
+    for t in reversed(range(0, T - 1)):
+        J[:, :, t] = np.linalg.solve(Ppre[:, :, t + 1], A @ Pcur[:, :, t]).T
+        Xfin[:, :, t] = Xcur[:, :, t] + (Xfin[:, :, t + 1] - Xcur[:, :, t] @ A.T) @ J[:, :, t].T
+
         Pfin[:, :, t] = Pcur[:, :, t] + J[:, :, t] @ (Pfin[:, :, t + 1] - Ppre[:, :, t + 1]) @ J[:, :, t].T
-        Pt = Pfin[:, :, t] + Xfin[:, :, t].T @ Xfin[:, :, t] / N  # Pt=Pfin(:,:,t) + Xfin(:,:,t)'*Xfin(:,:,t)/N;
-        Ptsum = Ptsum + Pt  # Ptsum=Ptsum+Pt;
-        YX = YX + Y[:, :, t].T @ Xfin[:, :, t]  # YX=YX+Y(:,:,t)'*Xfin(:,:,t);
-    # end;
+        Pt = Pfin[:, :, t] + Xfin[:, :, t].T @ Xfin[:, :, t] / N
+        Ptsum = Ptsum + Pt
+        YX = YX + Y[:, :, t].T @ Xfin[:, :, t]
 
-    A3 = Ptsum - Pt  # A3= Ptsum-Pt;
-    A2 = Ptsum + A2  # A2= Ptsum+A2;
+    A3 = Ptsum - Pt
+    A2 = Ptsum + A2
 
-    t = T - 1  # t=T;
-    Pcov = (I - KC) @ A @ Pcur[:, :, t - 1]  # Pcov=(I-KC)*A*Pcur(:,:,t-1);
-    A1 = A1 + Pcov + Xfin[:, :, t].T @ Xfin[:, :, t - 1] / N  # A1=A1+Pcov+Xfin(:,:,t)'*Xfin(:,:,t-1)/N;
+    t = T - 1
+    Pcov = (I - KC) @ A @ Pcur[:, :, t - 1]
+    A1 = A1 + Pcov + Xfin[:, :, t].T @ Xfin[:, :, t - 1] / N
 
-    for t in reversed(range(1, T - 1)):  # for t=(T-1):-1:2
-        Pcov = (Pcur[:, :, t] + J[:, :, t] @ (Pcov - A @ Pcur[:, :, t])) @ J[:, :, t - 1].T  # Pcov=(Pcur(:,:,t)+J(:,:,t)*(Pcov-A*Pcur(:,:,t)))*J(:,:,t-1)';
-        A1 = A1 + Pcov + Xfin[:, :, t].T @ Xfin[:, :, t - 1] / N  # A1=A1+Pcov+Xfin(:,:,t)'*Xfin(:,:,t-1)/N;
-    # end;
+    for t in reversed(range(1, T - 1)):
+        Pcov = (Pcur[:, :, t] + J[:, :, t] @ (Pcov - A @ Pcur[:, :, t])) @ J[:, :, t - 1].T
+        A1 = A1 + Pcov + Xfin[:, :, t].T @ Xfin[:, :, t - 1] / N
 
-    if problem:  # if problem
-        print('problem')  # fprintf(' problem  ');
-        problem = 0  # problem=0;
-    # end;
+    if problem:
+        print('problem')
+        problem = 0
 
     return lik, Xfin, Pfin, Ptsum, YX, A1, A2, A3
--- src/orig/lds.py
+++ src/orig/lds.py
@@ -1,77 +1,72 @@
 import numpy as np
 
-# Returns: net
 from src.orig.kalmansmooth import kalmansmooth
 
 
 
-def lds(X, K = 2, T = None, cyc = 100, tol = 0.0001):  # function net=lds(X,K,T,cyc,tol);
-    p = len(X[0, :])  # p=length(X(1,:));
-    N = len(X[:, 0])  # N=length(X(:,1));
-    if T is None:  # nargin stuff
+def lds(X, K = 2, T = None, cyc = 100, tol = 0.0001):
+    p = len(X[0, :])
+    N = len(X[:, 0])
+    if T is None:
         T = N
 
-    Mu = np.mean(X, axis = 0).reshape(1, -1)  # Mu=mean(X);
-    X = X - np.ones((N, 1)) @ Mu  # X=X-ones(N,1)*Mu;
+    Mu = np.mean(X, axis = 0).reshape(1, -1)
+    X = X - np.ones((N, 1)) @ Mu
 
-    if N % T != 0:  # if (rem(N,T)~=0)
-        # disp('Error: Data matrix length must be multiple of sequence length T');
+    if N % T != 0:
+
         print('Error: Data matrix length must be multiple of sequence length T')
-        return  # return;
-    # end;
+        return
 
-    N = N / T  # N=N/T;
+    N = N / T
 
-    # TODO: Replace to support other state/measurement sizes.
-    A = np.eye(K)  # A = eye(K);
-    Q = np.eye(K)  # Q = eye(K);
-    C = np.eye(p, K)  # C = eye(p, K);
-    R = np.ones(p)  # R = ones(p, 1);
-    x0 = np.ones((K, 1))  # x0 = ones(K, 1);
-    P0 = np.eye(K)  # P0 = eye(K);
+    A = np.eye(K)
+    Q = np.eye(K)
+    C = np.eye(p, K)
+    R = np.ones(p)
+    x0 = np.ones((K, 1))
+    P0 = np.eye(K)
 
-    lik = 0  # lik=0;
-    LL = []  # LL=[];
+    lik = 0
+    LL = []
 
-    Y = X.reshape(int(T), int(N), int(p))  # Y=reshape(X,T,N,p);
-    Y = np.transpose(Y, axes = [1, 2, 0])  # Y=permute(Y,[2 3 1]); % Y is (N,p,T), analogously to X
+    Y = X.reshape(int(T), int(N), int(p))
+    Y = np.transpose(Y, axes = [1, 2, 0])
 
-    YY = np.sum(np.multiply(X, X), axis = 0) / (T * N)  # YY=sum(X.*X)'/(T*N);
+    YY = np.sum(np.multiply(X, X), axis = 0) / (T * N)
 
     Q_problem = False
     R_problem = False
     P0_problem = False
-    for cycle in range(cyc):  # for cycle=1:cyc
+    for cycle in range(cyc):
         # E STEP
-        oldlik = lik  # oldlik=lik;
-        lik, Xfin, Pfin, Ptsum, YX, A1, A2, A3 = kalmansmooth(A, C, Q, R, x0, P0, Y)  # [lik,Xfin,Pfin,Ptsum,YX,A1,A2,A3]=kalmansmooth(A,C,Q,R,x0,P0,Y);
-        LL.append(lik)  # LL=[LL lik];
-        print('cycle %d lik %f' % (cycle, lik))  # fprintf('cycle %g lik %g',cycle,lik);
+        oldlik = lik
+        lik, Xfin, Pfin, Ptsum, YX, A1, A2, A3 = kalmansmooth(A, C, Q, R, x0, P0, Y)
+        LL.append(lik)
+        print('cycle %d lik %f' % (cycle, lik))
 
-        if cycle <= 1:  # if (cycle<=2)
-            likbase = lik  # likbase=lik;
-        elif lik < oldlik:  # elseif (lik<oldlik)
-            print('violation')  # fprintf(' violation');
-        elif (lik - likbase) < (1 + tol) * (oldlik - likbase) or not np.isfinite(lik):  # elseif ((lik-likbase)<(1 + tol)*(oldlik-likbase)|~isfinite(lik))
-            print()  # fprintf('\n');
-            break  # break;
-        # end;
+        if cycle <= 1:
+            likbase = lik
+        elif lik < oldlik:
+            print('violation')
+        elif (lik - likbase) < (1 + tol) * (oldlik - likbase) or not np.isfinite(lik):
+            print()
+            break
 
-        # M STEP
-        x0 = np.sum(Xfin[:, :, 0], axis = 0).reshape(1, -1).T / N  # x0=sum(Xfin(:,:,1),1)'/N;
-        T1 = Xfin[:, :, 0] - np.ones((int(N), 1)) @ x0.T  # T1=Xfin(:,:,1)-ones(N,1)*x0';
-        P0 = Pfin[:, :, 0] + T1.T @ T1 / N  # P0=Pfin(:,:,1)+T1'*T1/N;
-        C = np.linalg.solve(Ptsum, YX.T).T / N  # C=YX*inv(Ptsum)/N;
-        R = YY - np.diag(C @ YX.T) / (T * N)  # R=YY-diag(C*YX')/(T*N);
-        A = np.linalg.solve(A2, A1.T).T  # A=A1*inv(A2);
-        Q = (1 / (T - 1)) * np.diag(np.diag(A3 - A @ A1.T))  # Q=(1/(T-1))*diag(diag((A3-A*(A1'))));
-        if np.linalg.det(Q) < 0:  # if (det(Q)<0)
-            print('Q problem')  # fprintf('Q problem\n');
-        # end;
+
+            # M STEP
+        x0 = np.sum(Xfin[:, :, 0], axis = 0).reshape(1, -1).T / N
+        T1 = Xfin[:, :, 0] - np.ones((int(N), 1)) @ x0.T
+        P0 = Pfin[:, :, 0] + T1.T @ T1 / N
+        C = np.linalg.solve(Ptsum, YX.T).T / N
+        R = YY - np.diag(C @ YX.T) / (T * N)
+        A = np.linalg.solve(A2, A1.T).T
+        Q = (1 / (T - 1)) * np.diag(np.diag(A3 - A @ A1.T))
+        if np.linalg.det(Q) < 0:
+            print('Q problem')
 
         Q_problem = np.linalg.det(Q) < 0
         R_problem = np.linalg.det(np.diag(R)) < 0
         P0_problem = np.linalg.det(P0) < 0
-    # end;
 
     return A, Q, C, R, x0, P0, LL, Q_problem, R_problem, P0_problem
