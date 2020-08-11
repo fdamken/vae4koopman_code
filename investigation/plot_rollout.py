844815a8d1904190684966a80b474dd452732c70
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2020-08-11 17:27:51 +0200

    Enhanced rollout plot.

--- investigation/plot_rollout.py
+++ investigation/plot_rollout.py
@@ -2,15 +2,18 @@ from typing import List
 
 import numpy as np
 
-from investigation.plot_util import SubplotsAndSave
+from investigation.plot_util import SubplotsAndSave, tuda
 from investigation.util import ExperimentConfig, ExperimentResult, load_run
 
 
+PREDICTION_STEPS = 500
 
-def compute_rollout(config: ExperimentConfig, result: ExperimentResult) -> np.ndarray:
-    rollout = np.zeros((config.T, config.latent_dim))
+
+
+def compute_rollout(config: ExperimentConfig, result: ExperimentResult, T: int) -> np.ndarray:
+    rollout = np.zeros((T, config.latent_dim))
     rollout[0, :] = result.m0
-    for t in range(1, config.T):
+    for t in range(1, T):
         rollout[t, :] = result.A @ rollout[t - 1, :]
     return rollout
 
@@ -21,8 +24,14 @@ def computer_observations(result: ExperimentResult, latent_trajectory: np.ndarra
 
 
 
-def plot_latent_rollout(config: ExperimentConfig, result: ExperimentResult, out_dir: str, latent_trajectories: List[np.ndarray]):
-    domain = np.arange(config.T) * config.h
+def plot_latent_rollout(config: ExperimentConfig, result: ExperimentResult, out_dir: str, prediction_steps: int, latent_trajectories: List[np.ndarray]):
+    T, _ = latent_trajectories[0].shape
+    domain = np.arange(T) * config.h
+    if prediction_steps > 0:
+        domain_before_prediction = domain[:-prediction_steps]
+    else:
+        domain_before_prediction = domain
+    domain_predicted = domain[-prediction_steps:]
 
     with SubplotsAndSave(out_dir, 'rollout-latents', config.N, config.latent_dim,
                          sharex = 'all',
@@ -30,9 +39,16 @@ def plot_latent_rollout(config: ExperimentConfig, result: ExperimentResult, out_
                          figsize = (2 + 5 * config.latent_dim, 1 + 4 * config.N),
                          squeeze = False) as (fig, axss):
         for n, (axs, latent_trajectory) in enumerate(zip(axss, latent_trajectories)):
+            if prediction_steps > 0:
+                latent_trajectory_before_prediction = latent_trajectory[:-prediction_steps, :]
+            else:
+                latent_trajectory_before_prediction = latent_trajectory
+            latent_trajectory_predicted = latent_trajectory[-prediction_steps:, :]
             for dim, ax in enumerate(axs):
-                ax.plot(domain, result.estimations_latents[n, dim, :], label = 'Filtered/Smoothed')
-                ax.plot(domain, latent_trajectory[:, dim], label = 'Rollout')
+                ax.plot(domain_before_prediction, result.estimations_latents[n, dim, :], color = tuda('orange'), label = 'Filtered/Smoothed')
+                ax.plot(domain_before_prediction, latent_trajectory_before_prediction[:, dim], color = tuda('blue'), label = 'Rollout')
+                if prediction_steps > 0:
+                    ax.plot(domain_predicted, latent_trajectory_predicted[:, dim], color = tuda('blue'), ls = 'dashed', label = 'Rollout (Prediction)')
                 ax.set_title('Trajectory %d, Dim. %d' % (n + 1, dim + 1))
                 ax.set_xlabel('Time Steps')
                 ax.set_ylabel('Latents')
@@ -41,8 +57,14 @@ def plot_latent_rollout(config: ExperimentConfig, result: ExperimentResult, out_
 
 
 
-def plot_observations_rollout(config: ExperimentConfig, result: ExperimentResult, out_dir: str, observation_trajectories: List[np.ndarray]):
-    domain = np.arange(config.T) * config.h
+def plot_observations_rollout(config: ExperimentConfig, result: ExperimentResult, out_dir: str, prediction_steps: int, observation_trajectories: List[np.ndarray]):
+    T, _ = observation_trajectories[0].shape
+    domain = np.arange(T) * config.h
+    if prediction_steps > 0:
+        domain_before_prediction = domain[:-prediction_steps]
+    else:
+        domain_before_prediction = domain
+    domain_predicted = domain[-prediction_steps:]
 
     observation_trajectories_smoothed = result.g_numpy(result.estimations_latents.transpose((0, 2, 1)).reshape(-1, config.latent_dim)).reshape(
             (config.N, config.T, config.observation_dim))
@@ -57,11 +79,19 @@ def plot_observations_rollout(config: ExperimentConfig, result: ExperimentResult
                 mean = observation_trajectory[:, dim]
                 upper = mean + confidence
                 lower = mean - confidence
+                if prediction_steps > 0:
+                    mean_before_prediction = mean[:-prediction_steps]
+                else:
+                    mean_before_prediction = mean
+                mean_predicted = mean[-prediction_steps:]
 
-                ax.plot(domain, result.observations[n, :, dim], label = 'Truth')
-                line = ax.plot(domain, mean, label = 'Rollout')[0]
-                ax.plot(domain, observation_trajectory_smoothed[:, dim], ls = 'dashdot', label = 'Smoothed/Filtered')
-                ax.fill_between(domain, upper, lower, where = upper > lower, color = line.get_color(), alpha = 0.2, label = 'Rollout Confidence')
+                ax.scatter(domain_before_prediction, result.observations[n, :, dim], s = 1, color = tuda('black'), label = 'Truth')
+                ax.plot(domain_before_prediction, mean_before_prediction, color = tuda('blue'), label = 'Rollout')
+                ax.plot(domain_predicted, mean_predicted, color = tuda('blue'), ls = 'dashed', label = 'Rollout (Prediction)')
+                # ax.plot(domain, observation_trajectory_smoothed[:, dim], ls = 'dashdot', label = 'Smoothed/Filtered')
+                ax.fill_between(domain, upper, lower, where = upper > lower, color = tuda('blue'), alpha = 0.2, label = 'Rollout Confidence')
+                if prediction_steps > 0:
+                    ax.axvline(domain_before_prediction[-1], color = tuda('red'), ls = 'dotted', label = 'Prediction Boundary')
                 ax.set_title('Trajectory %d, Dim. %d' % (n + 1, dim + 1))
                 ax.set_xlabel('Time Steps')
                 ax.set_ylabel('Observations')
@@ -74,18 +104,18 @@ def plot_rollout(out_dir, config: ExperimentConfig, result: ExperimentResult):
     latent_trajectories = []
     observation_trajectories = []
     for _ in range(config.N):
-        latent_trajectory = compute_rollout(config, result)
+        latent_trajectory = compute_rollout(config, result, config.T + PREDICTION_STEPS)
         observation_trajectory = computer_observations(result, latent_trajectory)
         latent_trajectories.append(latent_trajectory)
         observation_trajectories.append(observation_trajectory)
 
-    plot_latent_rollout(config, result, out_dir, latent_trajectories)
-    plot_observations_rollout(config, result, out_dir, observation_trajectories)
+    plot_latent_rollout(config, result, out_dir, PREDICTION_STEPS, latent_trajectories)
+    plot_observations_rollout(config, result, out_dir, PREDICTION_STEPS, observation_trajectories)
 
 
 
 if __name__ == '__main__':
     out_dir = 'investigation/tmp_figures'
-    config, result, _ = load_run('tmp_results/transferred_results/27', 'run', 'metrics')
+    config, result, _ = load_run('tmp_results/transferred_results/29', 'run', 'metrics')
 
     plot_rollout(out_dir, config, result)
--- investigation/plot_util.py
+++ investigation/plot_util.py
@@ -4,6 +4,22 @@ from typing import List, Optional
 import matplotlib.pyplot as plt
 
 
+TUDA_COLORS = {
+        '0':  { 'a': '#DCDCDC', 'b': '#B5B5B5', 'c': '#898989', 'd': '#535353' },
+        '1':  { 'a': '#5D85C3', 'b': '#005AA9', 'c': '#004E8A', 'd': '#243572' },
+        '2':  { 'a': '#009CDA', 'b': '#0083CC', 'c': '#00689D', 'd': '#004E73' },
+        '3':  { 'a': '#50B695', 'b': '#009D81', 'c': '#008877', 'd': '#00715E' },
+        '4':  { 'a': '#AFCC50', 'b': '#99C000', 'c': '#7FAB16', 'd': '#6A8B22' },
+        '5':  { 'a': '#DDDF48', 'b': '#C9D400', 'c': '#B1BD00', 'd': '#99A604' },
+        '6':  { 'a': '#FFE05C', 'b': '#FDCA00', 'c': '#D7AC00', 'd': '#AE8E00' },
+        '7':  { 'a': '#F8BA3C', 'b': '#F5A300', 'c': '#D28700', 'd': '#BE6F00' },
+        '8':  { 'a': '#EE7A34', 'b': '#EC6500', 'c': '#CC4C03', 'd': '#A94913' },
+        '9':  { 'a': '#E9503E', 'b': '#E6001A', 'c': '#B90F22', 'd': '#961C26' },
+        '10': { 'a': '#C9308E', 'b': '#A60084', 'c': '#951169', 'd': '#732054' },
+        '11': { 'a': '#804597', 'b': '#721085', 'c': '#611C73', 'd': '#4C226A' }
+}
+
+
 
 class SubplotsAndSave:
     _out_dir: str
@@ -33,3 +49,32 @@ class SubplotsAndSave:
         for file_type in self._file_types:
             self._fig.savefig('%s/%s.%s' % (self._out_dir, self._file_name, file_type))
         plt.close(self._fig)
+
+
+
+def tuda(code: str):
+    code = code.lower()
+    if len(code) == 2:
+        hue_code = code[0]
+        brightness_code = code[1]
+        if hue_code not in TUDA_COLORS:
+            raise Exception('Unknown TUDa hue code %s!' % hue_code)
+        if brightness_code not in TUDA_COLORS[hue_code]:
+            raise Exception('Unknown TUDa brightness code %s for hue code %s!' % (brightness_code, hue_code))
+        return TUDA_COLORS[hue_code][brightness_code]
+    elif code == 'blue':
+        return tuda('2b')
+    elif code == 'orange':
+        return tuda('7b')
+    elif code == 'green':
+        return tuda('3b')
+    elif code == 'red':
+        return tuda('9b')
+    elif code == 'purple':
+        return tuda('11b')
+    elif code == 'pink':
+        return tuda('11a')
+    elif code == 'gray':
+        return tuda('0c')
+    elif code == 'black':
+        return '#000000'
