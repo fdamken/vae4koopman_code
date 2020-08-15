955adc82a1fa203b6febbe9bf747106d1ddedd16
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2020-08-15 16:35:42 +0200

    Added option to treat cov as sqrt(cov) for speedup.

--- src/cubature.py
+++ src/cubature.py
@@ -10,31 +10,41 @@ import torch
 from src.util import mlib_square
 
 
+_xi_cache = { 'torch': { }, 'np': { } }
 
-def spherical_radial(n: int, f: Callable[[np.ndarray], np.ndarray], mean: np.ndarray, cov: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+
+
+def spherical_radial(n: int, f: Callable[[np.ndarray], np.ndarray], mean: np.ndarray, cov: np.ndarray, cov_is_sqrt: bool = False) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
     Computes the spherical radial cubature rule for gaussian probability density functions, i.e. the expectation value of ``f``
     with a gaussian distribution with mean ``mean`` and covariance matrix ``cov``.
 
     :param n: The dimension of the gaussian.
-    :param f: Accepts shape ``(2n, n)``, produces shape ``(2n, m)``; The function to approximate the expectation of. Has to
-            accept a batch of points where axis ``0`` is the batch index and axis ``1`` is the dimensionality the same axis
-            definitions must hold for the result.
+    :param f: Accepts shape ``(k, n)``, produces shape ``(k, m)``; The function to approximate the expectation of. Has to
+              accept a batch ``k`` of points where axis ``0`` is the batch index and axis ``1`` is the dimensionality the
+              same axis definitions must hold for the result.
     :param mean: Shape ``(b,n)``; The mean of the gaussian distribution. Axis ``0`` is the batch axis.
     :param cov: Shape ``(b, n, n)``; The covariance matrix of the gaussian distribution. Axis ``0`` is the batch axis.
+    :param cov_is_sqrt: Sets whether the given ``cov`` is really a covariance matrix (``False``) or already is the principal
+                        square root of the covariance matrix (``True``).
+    :return: Tuple ``(result, cubature_points, L)``:
+                 - ``result``: Shape matches the result of ``f`` along with axis ``0`` as the batch axis.; The approximation of the expectation value.
+                 - ``cubature_points``: Shape ``(2n, n)``; The cubature points used for approximating the expected value.
+                 - ``L``: The principal square root of ``cov``. Can be used to speed up other cubature evaluations.
     :return: Shape matches the result of ``f`` along with axis ``0`` as the batch axis.; The approximation of the expectation value.
     """
 
-    return _spherical_radial(False, n, f, mean, cov)
+    return _spherical_radial(False, n, f, mean, cov, cov_is_sqrt)
 
 
 
-def spherical_radial_torch(n: int, f: Callable[[torch.Tensor], torch.Tensor], mean: torch.Tensor, cov: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+def spherical_radial_torch(n: int, f: Callable[[torch.Tensor], torch.Tensor], mean: torch.Tensor, cov: torch.Tensor, cov_is_sqrt: bool = False) \
+        -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """
     PyTorch version of ``spherical_radial(..)``. See there for documentation.
     """
 
-    return _spherical_radial(True, n, f, mean, cov)
+    return _spherical_radial(True, n, f, mean, cov, cov_is_sqrt)
 
 
 
@@ -65,11 +75,20 @@ def _xi(use_torch: bool, n: int, device: Optional[torch.device]) -> Union[torch.
 
 
 def _spherical_radial(use_torch: bool, n: int, f: Callable[[Union[np.ndarray, torch.Tensor]], Union[np.ndarray, torch.Tensor]], mean: Union[np.ndarray, torch.Tensor],
-                      cov: Union[np.ndarray, torch.Tensor]) -> Tuple[Union[np.ndarray, torch.Tensor], Union[np.ndarray, torch.Tensor]]:
-    cov_np = cov.detach().cpu().numpy() if use_torch else cov
-    L_np = [scipy.linalg.sqrtm(it).astype(np.float) for it in cov_np]
-    L = torch.tensor(L_np, dtype = cov.dtype, device = cov.device) if use_torch else np.asarray(L_np)
-    xi = _xi(use_torch, n, device = mean.device if use_torch else None)
+                      cov: Union[np.ndarray, torch.Tensor], cov_is_sqrt: bool) \
+        -> Tuple[Union[np.ndarray, torch.Tensor], Union[np.ndarray, torch.Tensor], Union[np.ndarray, torch.Tensor]]:
+    if n in _xi_cache['torch' if use_torch else 'np']:
+        xi = _xi_cache['torch' if use_torch else 'np'][n]
+    else:
+        xi = _xi(use_torch, n, device = mean.device if use_torch else None)
+        _xi_cache['torch' if use_torch else 'np'][n] = xi
+
+    if cov_is_sqrt:
+        L = cov
+    else:
+        cov_np = cov.detach().cpu().numpy() if use_torch else cov
+        L_np = [scipy.linalg.sqrtm(it).astype(np.float) for it in cov_np]
+        L = torch.tensor(L_np, dtype = cov.dtype, device = cov.device) if use_torch else np.asarray(L_np)
     if use_torch:
         cubature_points = torch.einsum('ij,bjk->bik', xi, L) + mean.unsqueeze(1)
         f_eval = f(cubature_points.reshape(-1, mean.shape[1]))
@@ -77,8 +96,9 @@ def _spherical_radial(use_torch: bool, n: int, f: Callable[[Union[np.ndarray, to
     else:
         cubature_points = np.einsum('ij,bjk->bik', xi, L) + mean[:, np.newaxis, :]
         f_eval = f(cubature_points.reshape(-1, mean.shape[1]))
+        # noinspection PyArgumentList
         result_sum = f_eval.reshape((cubature_points.shape[0], cubature_points.shape[1], *f_eval[0].shape)).sum(axis = 1)
-    return result_sum / (2 * n), cubature_points
+    return result_sum / (2 * n), cubature_points, L
 
 
 
@@ -97,9 +117,9 @@ def _demo():
                                    torch.mul(x[:, 0], torch.sin(x[:, 1])).view(-1, 1)], dim = 1)
 
     # NumPy version.
-    approx_mean_batch_np, cubature_points_batch_np = spherical_radial(n, f, mean_batch, cov_batch)
+    approx_mean_batch_np, cubature_points_batch_np, _ = spherical_radial(n, f, mean_batch, cov_batch)
     # PyTorch version.
-    approx_mean_batch, cubature_points_batch = spherical_radial_torch(n, f_torch, mean_batch_t, cov_batch_t)
+    approx_mean_batch, cubature_points_batch, _ = spherical_radial_torch(n, f_torch, mean_batch_t, cov_batch_t)
     approx_mean_batch = approx_mean_batch.numpy()
     cubature_points_batch = cubature_points_batch.numpy()
     # The above is calculated twice. This is useful for development to keep both implementations in sync.
