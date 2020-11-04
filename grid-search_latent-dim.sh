f1b1cc95e36923f34fa6ec2ae0f2d4ffef3d0a01
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2020-11-04 17:06:03 +0100

    Added grid search script, removed lots of old experiment files.

---
+++ grid-search_latent-dim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+experiment="$1"
+if [[ "$experiment" == "" ]]; then
+    echo "E: Usage: $0 <experiment-name>" >&2
+    exit 126
+fi
+
+set -o errexit
+set -o nounset
+
+results_dir="tmp_results_grid_search/latent-dim_$(date +%Y%m%dT%H:%M:%S)"
+log_dir="$results_dir/log"
+mkdir -p "$results_dir" "$log_dir"
+
+run_ex() {
+    latent_dim="$1"
+    PYTHONPATH=. RESULTS_DIR="$results_dir" python src/experiment.py "$experiment" "latent_dim=$latent_dim" # &>>"$log_dir/run-$latent_dim"
+}
+
+echo "Running multiple latent dims for experiment $experiment."
+
+for latent_dim in $(seq 1 100); do
+    set +o errexit
+    run_ex "$latent_dim"
+    set -o errexit
+done
